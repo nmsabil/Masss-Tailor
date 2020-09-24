e65a6cceb94392d96808c2125ab31ee025a0b3fb
Sabil <sabilmdtv1@gmail.com>
2020-09-24 20:25:39 +0100

    fixed the navbar spyscroll

--- script.js
+++ script.js
@@ -19,54 +19,12 @@ var openOrClosed = document.getElementById("openorclosed");
 var serviceLink = document.querySelectorAll("#servicelink");
 var serviceTitleInside = document.querySelectorAll("#test");
 
-$(document).ready(function () {
-  $(document).on("scroll", onScroll);
+const menu = document.querySelector(".navbar");
+const scrollspy = new VanillaScrollspy(menu, 1000);
+scrollspy.init();
 
-  //smoothscroll
-  $('a[href^="#"]').on("click", function (e) {
-    e.preventDefault();
-    $(document).off("scroll");
-
-    $("a").each(function () {
-      $(this).removeClass("active");
-    });
-    $(this).addClass("active");
-
-    var target = this.hash,
-      menu = target;
-    $target = $(target);
-    $("html, body")
-      .stop()
-      .animate(
-        {
-          scrollTop: $target.offset().top + 2,
-        },
-        500,
-        "swing",
-        function () {
-          window.location.hash = target;
-          $(document).on("scroll", onScroll);
-        }
-      );
-  });
-});
-
-function onScroll(event) {
-  var scrollPos = $(document).scrollTop();
-  $("#menu-center a").each(function () {
-    var currLink = $(this);
-    var refElement = $(currLink.attr("href"));
-    if (
-      refElement.position().top <= scrollPos &&
-      refElement.position().top + refElement.height() > scrollPos
-    ) {
-      $("#menu-center ul li a").removeClass("active");
-      currLink.addClass("active");
-    } else {
-      currLink.removeClass("active");
-    }
-  });
-}
+var y = window.scrollY;
+console.log(y);
 
 // preloader animation
 window.onload = function () {
